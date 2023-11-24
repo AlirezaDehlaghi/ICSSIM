3b19936f0a971c4b55ae3ed8966c319ab2d4b614
alirezade <alirezade@WPPC1N9X16>
2023-11-24 17:34:54 +0100

    Documentation1

---
+++ example.py
@@ -0,0 +1,22 @@
+def multiply(a: float, b: float) -> float:
+    """
+    Multiply two numbers.
+
+    :param a: First number.
+    :param b: Second number.
+    :return: The product of a and b.
+    """
+    return a * b
+
+def print_all():
+    """
+    it print every thing
+
+    :return: The product of a and b.
+    """
+    
+    print ("Hi")
+    return "Hi"
+
+
+print_all()
