ec997f560eaf8970569cdfd26b3b44f9960f7844
alirezade <alirezade@WPPC1N9X16>
2023-11-24 17:37:53 +0100

    Documentation2

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
