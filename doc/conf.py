7d40b0ee58fd558366567f4c3b11271dfbd00aef
alirezade <alirezade@WPPC1N9X16>
2023-11-25 00:58:38 +0100

    Documentation7

--- doc/conf.py
+++ doc/conf.py
@@ -9,6 +9,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath("../src"))
+sys.path.insert(1, os.path.abspath("../src/ics_sim"))
 
 project = 'ICSSIM'
 copyright = 'Alireza Dehlaghi Ghadim'
